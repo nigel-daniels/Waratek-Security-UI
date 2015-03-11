2fedaff2b9903267e4a8534156e619e1cfafca5a
nigel.daniels <nigel.daniels@axiomode.com>
2015-03-10 17:49:26 -0700

    The new abstract lines class

---
+++ com.waratek.rules.ui.emf/src/com/waratek/rules/Lines.java
@@ -0,0 +1,27 @@
+/**
+ * Copyright 2015 Waratek Ltd.
+ */
+package com.waratek.rules;
+
+import org.eclipse.emf.ecore.EObject;
+
+/**
+ * <!-- begin-user-doc -->
+ * A representation of the model object '<em><b>Lines</b></em>'.
+ * <!-- end-user-doc -->
+ *
+ *
+ * @see com.waratek.rules.RulesPackage#getLines()
+ * @model abstract="true"
+ * @generated
+ */
+public interface Lines extends EObject
+{
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	String copyright = "Copyright 2015 Waratek Ltd.";
+
+} // Lines
---
+++ com.waratek.rules.ui.emf/src/com/waratek/rules/impl/LinesImpl.java
@@ -0,0 +1,52 @@
+/**
+ * Copyright 2015 Waratek Ltd.
+ */
+package com.waratek.rules.impl;
+
+import com.waratek.rules.Lines;
+import com.waratek.rules.RulesPackage;
+
+import org.eclipse.emf.ecore.EClass;
+
+import org.eclipse.emf.ecore.impl.MinimalEObjectImpl;
+
+/**
+ * <!-- begin-user-doc -->
+ * An implementation of the model object '<em><b>Lines</b></em>'.
+ * <!-- end-user-doc -->
+ * <p>
+ * </p>
+ *
+ * @generated
+ */
+public abstract class LinesImpl extends MinimalEObjectImpl.Container implements Lines
+{
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public static final String copyright = "Copyright 2015 Waratek Ltd.";
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected LinesImpl()
+	{
+		super();
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	protected EClass eStaticClass()
+	{
+		return RulesPackage.Literals.LINES;
+	}
+
+} //LinesImpl
